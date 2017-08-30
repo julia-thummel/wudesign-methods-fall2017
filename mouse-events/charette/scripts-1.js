ca06711134db466b5dd2864287a374a5e5b83726
Julia Thummel <julia.thummel@gmail.com>
2017-08-30 11:22:24 -0500

    Add files via upload

--- mouse-events/charette/scripts-1.js
+++ mouse-events/charette/scripts-1.js
@@ -5,27 +5,23 @@ var handler = function() {}
         $(".one").click(function(){
           $(this).toggleClass("selected");
         });
-    });
 
-    $(document).ready(function(){
+
         $(".two").click(function(){
           $(this).toggleClass("selected");
         });
-    });
 
-    $(document).ready(function(){
+
         $(".three").click(function(){
           $(this).toggleClass("selected");
         });
-    });
 
-    $(document).ready(function(){
+
         $(".four").click(function(){
           $(this).toggleClass("selected");
         });
-    });
 
-    $(document).ready(function(){
+
         $(".five").click(function(){
           $(this).toggleClass("selected");
         });
