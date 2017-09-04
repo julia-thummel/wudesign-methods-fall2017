42560a639be4017e64e4fa3c7328fd438dcddd0b
Julia Thummel <julia.thummel@gmail.com>
2017-09-04 15:22:18 -0500

    Add files via upload

--- mouse-events/charette/scripts-2.js
+++ mouse-events/charette/scripts-2.js
@@ -1,8 +1,118 @@
-function named() {}
-var handler = function() {}
+// function named() {}
+// var handler = function() {}
 
     $(document).ready(function(){
         $(".one").click(function(){
+          console.log("hello there");
+        });
+
+        $(".five").click(function(){
           $(this).toggleClass("selected");
         });
+
+        $(".six").click(function(){
+          $(this).toggleClass("selected");
+        });
+
+        $(".seven").click(function(){
+          $(this).toggleClass("selected");
+        });
+
+        $(".textChangeOne").click(function(){
+          $(this).toggleClass("selected");
+        });
+
+        $(".textChangeTwo").click(function(){
+          $(this).toggleClass("selected");
+        });
+
+        $(".ten").click(function(){
+          $(this).toggleClass("selected");
+        });
+
+        $(".eleven").click(function(){
+          $(this).toggleClass("selected");
+        });
+
+        $(".textChangeThree").click(function(){
+          $(this).toggleClass("selected");
+        });
+
+        $(".thirteen").click(function(){
+          $(this).toggleClass("selected");
+        });
+
+        $(".fourteen").click(function(){
+          $(this).toggleClass("selected");
+        });
+
+        $(".fifteen").click(function(){
+          $(this).toggleClass("selected");
+        });
+
+        $(".sixteen").click(function(){
+          $(this).toggleClass("selected");
+        });
+
+        $(".seventeen").click(function(){
+          $(this).toggleClass("selected");
+        });
+
+        $(".eighteen").click(function(){
+          $(this).toggleClass("selected");
+        });
+
+        $(".nineteen").click(function(){
+          $(this).toggleClass("selected");
+        });
+
+        $(".twenty").click(function(){
+          $(this).toggleClass("selected");
+        });
+
+        $(".twentyone").click(function(){
+          var x = 5;
+          var y = 2;
+          var z = x + y;
+          document.getElementById("sum").innerHTML = z;
+        });
+
+        $(".twentytwo").click(function(){
+          document.getElementById("date").innerHTML = Date();
+        });
+
+        $(".twentythree").click(function(){
+          $(this).toggleClass("selected");
+        });
+
+        $(".twentyfour").click(function(){
+          $(this).toggleClass("selected");
+        });
+
     });
+
+    function oneFunction() {
+      alert("hello there");
+    }
+
+    function twoFunction() {
+    var txt;
+    if (confirm("Press a button!") == true) {
+        txt = ":)";
+    } else {
+        txt = ":(";
+    }
+    document.getElementById("reaction").innerHTML = txt;
+}
+
+
+  function threeFunction() {
+      var txt;
+      var person = prompt("Please enter your name:", "Abe");
+      if (person == null || person == "") {
+          txt = "User cancelled the prompt.";
+      } else {
+          txt = "Hello " + person + "!";
+      }
+      document.getElementById("greeting").innerHTML = txt;
+  }
---
+++ mouse-events/charette/scripts-3.js
@@ -0,0 +1,19 @@
+function oneFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function twoFunction() {
+    document.getElementById("genButton").classList.toggle("gen");
+}
+
+function threeFunction() {
+    document.getElementById("genImage").classList.toggle("gen");
+}
+
+function fourFunction() {
+    document.getElementById("placeImage").classList.toggle("showOne");
+}
+
+function fiveFunction() {
+    document.getElementById("placeImage").classList.toggle("showOne");
+}
