399b0783015645fe3825a30b3e12e3f36eba5250
Julia Thummel <julia.thummel@gmail.com>
2017-09-25 02:12:15 -0500

    Add files via upload

---
+++ mouse-events/round-3/scripts-9.js
@@ -0,0 +1,11 @@
+$(document).ready( function() {
+
+  $('#deletesuccess').delay(4000).fadeOut();
+
+  $('#start').hide().delay(4500).fadeIn();
+
+  $(".bubble").mousedown(function() {
+    $(this).css('background-color', 'white');
+  )};
+
+});
