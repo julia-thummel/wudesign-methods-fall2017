e44f01fde13484b96f7bad413c04e7e9f23520fd
Julia Thummel <julia.thummel@gmail.com>
2017-09-25 09:26:30 -0500

    Add files via upload

---
+++ mouse-events/round-4/scripts-1.js
@@ -0,0 +1,16 @@
+$(document).ready( function() {
+
+  $('#deletesuccess').delay(4000).fadeOut();
+
+  $('#start').hide().delay(4500).fadeIn();
+  //
+  // $(".bubble").on("mousedown", function() {
+  //   $(this).css('background', 'white');
+  // });
+
+  // $(".bubble").on("mouseup", function() {
+  //   $(this).css('background', 'linear-gradient(-45deg, #EE7752, #E73C7E, #23A6D5, #23D5AB)');
+  // });
+
+
+});
