c7eaf52651e86f20d72a50e154ed3fc2884581f1
Julia Thummel <julia.thummel@gmail.com>
2017-09-25 10:03:57 -0500

    Add files via upload

--- mouse-events/round-4/scripts-1.js
+++ mouse-events/round-4/scripts-1.js
@@ -3,6 +3,42 @@ $(document).ready( function() {
   $('#deletesuccess').delay(4000).fadeOut();
 
   $('#start').hide().delay(4500).fadeIn();
+
+  $('#start').mousedown( function() {
+    $('#start').delay(500).fadeOut();
+  });
+
+
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+     var color = '#F';
+    for (var i = 0; i < 5; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+     return color;
+    };
+
+
+  $(".card1").flip({
+    axis: 'y',
+    trigger: 'manual'
+  });
+
+  $(".card2").flip({
+    axis: 'x',
+    trigger: 'manual'
+  });
+
+  $(".bubble").on("click", function () {
+    setInterval(function(){
+    $(".card:not(.flipped)").first().flip(true)
+    $(".card:not(.flipped)").first().addClass("flipped")
+  }, 3);
+    $(".back").each(function() {
+      $(this).css("background-color", getRandomColor({hue: 'light blue'}));
+    })
+  });
+
   //
   // $(".bubble").on("mousedown", function() {
   //   $(this).css('background', 'white');
