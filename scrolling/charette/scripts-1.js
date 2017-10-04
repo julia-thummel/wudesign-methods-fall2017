200c794897f72d6b159dbfee98876f7ed3a222ca
Julia Thummel <julia.thummel@gmail.com>
2017-10-03 19:13:39 -0500

    Add files via upload

---
+++ scrolling/charette/scripts-1.js
@@ -0,0 +1,20 @@
+$(document).ready( function() {
+  $(".square").each( function() {
+    $(this).css("background-color", randomColor({hue: 'blue'}));
+  })
+  $(".stay1").each( function() {
+    $(this).css("background-color", randomColor({hue: 'blue'}));
+  })
+  $(".stay2").each( function() {
+    $(this).css("background-color", randomColor({hue: 'blue'}));
+  })
+  $(".stay3").each( function() {
+    $(this).css("background-color", randomColor({hue: 'blue'}));
+  })
+  $(".splitStay2").each( function() {
+    $(this).css("background-color", randomColor({hue: 'blue'}));
+  })
+  $(".text").each( function() {
+    $(this).css("background-color", randomColor({hue: 'blue'}));
+  })
+})
