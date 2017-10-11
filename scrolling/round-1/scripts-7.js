c75c1dae0d0a78a5bdf29a2ee66244674c98bf95
Julia Thummel <julia.thummel@gmail.com>
2017-10-10 19:14:13 -0500

    Add files via upload

--- scrolling/round-1/scripts-7.js
+++ scrolling/round-1/scripts-7.js
@@ -1,33 +1,62 @@
 $(document).ready(function(){
 
-  $(".circle").each( function() {
-    var size = (Math.random())*150;
-    $(this).css("background-color", randomColor({hue: 'yellow'}));
-    $(this).css("left", (Math.random())*1000);
-    $(this).css("top", (Math.random())*1000);
-    $(this).css("width", size);
-    $(this).css("height", size);
+
+function style() {
+  $(".unstyled").each( function() {
+    $(this).css("background-color", randomColor({hue: 'red'}));
+    $(this).css("width", 0);
+    $(this).css("height", 0);
+    $(this).data("scrollSpeed", (Math.random())*6);
+    $(this).removeClass("unstyled");
   })
+};
+
+style();
 
 
 $(window).scroll(function() {
 
-// 1
-    $(".circle").each( function() {
-      var size = (Math.random())*200;
-      $(this).css("background-color", randomColor({hue: 'red'}));
-      $(this).animate({left: (Math.random())*1200}, {duration: 500, queue: false});
-      $(this).animate({top: (Math.random())*600}, {duration: 500, queue: false});
-      $(this).animate({width: size}, {duration: 500, queue: false});
-      $(this).animate({height: size}, {duration: 500, queue: false});
-    })
+  var left = $(window).scrollTop();
+  var right = ($(window).scrollTop() * -1);
+  var size = $(window).scrollTop();
+
+  $(".circle1").each( function() {
+    // $(this).css("left", left);
+    // $(this).css("width", 1/size);
+    // $(this).css("height", 1/size);
+    // $(this).css("background-color", randomColor({hue: 'red'}));
+    var speed = $(this).data("scrollSpeed");
+    var pos = -($(window).scrollTop()*(speed));
+    $(this).css("left" , pos + "px");
+    // $(this).animate({left: left}, {duration: Math.random()*500, queue: false}).delay(10000*Math.random());
+    $(this).css("width", size/2);
+    $(this).css("height", size/2);
+    $(this).css("opacity", left/500);
+    // $(this).animate({transform: scale(size)}, {duration: 500, queue: false});
+    // $(this).animate({height: size}, {duration: 500, queue: false});
+  });
+
+    $(".circle2").each( function() {
+      // $(this).css("left", left);
+      // $(this).css("width", 1/size);
+      // $(this).css("height", 1/size);
+      // $(this).css("background-color", randomColor({hue: 'red'}));
+      var speed = $(this).data("scrollSpeed");
+      var pos = -($(window).scrollTop()*(speed));
+      $(this).css("right" , pos + "px");
+      // $(this).animate({left: right}, {duration: Math.random()*500, queue: false}).delay(10000*Math.random());
+      $(this).css("width", size/2);
+      $(this).css("height", size/2);
+      $(this).css("opacity", left/500);
+      // $(this).animate({transform: scale(size)}, {duration: 500, queue: false});
+      // $(this).animate({height: size}, {duration: 500, queue: false});
+    });
 
 
     if ($(document).scrollTop() > 100) {
-      $(".container1").append("<div class='circle'></div>");
-    }
-    else {
-      $(".container1").remove("<div class='circle'></div>");
+      $(".container1").append("<div class='circle1 unstyled'></div>");
+      $(".container1").append("<div class='circle2 unstyled'></div>");
+      style();
     }
 
   });
---
+++ scrolling/round-1/scripts-8.js
@@ -0,0 +1,5 @@
+$(document).ready( function() {
+  $(".circle").each( function() {
+    $(this).css("background-color", randomColor({hue: 'yellow'}));
+  });
+});
