5b30d4d45c32140e48ab2cfda55ae5d0a5c5752c
Julia Thummel <julia.thummel@gmail.com>
2017-10-17 23:13:44 -0500

    Add files via upload

--- scrolling/round-2/scripts-7.js
+++ scrolling/round-2/scripts-7.js
@@ -9,6 +9,15 @@ $(document).ready(function(){
   animationSeven = document.getElementById("animation-seven");
   animationEight = document.getElementById("animation-eight");
 
+  $("#one").css("fill", randomColor({hue: 'blue'}));
+  $("#two").css("fill", randomColor({hue: 'blue'}));
+  $("#three").css("fill", randomColor({hue: 'blue'}));
+  $("#four").css("fill", randomColor({hue: 'blue'}));
+  $("#five").css("fill", randomColor({hue: 'blue'}));
+  $("#six").css("fill", randomColor({hue: 'blue'}));
+  $("#seven").css("fill", randomColor({hue: 'blue'}));
+  $("#eight").css("fill", randomColor({hue: 'blue'}));
+
 
   $(window).scroll(function() {
     $(".new").each( function() {
@@ -21,16 +30,6 @@ $(document).ready(function(){
         animationSeven.beginElement();
         animationEight.beginElement();
       })
-
-
-    $("#one").css("fill", randomColor({hue: 'blue'}));
-    $("#two").css("fill", randomColor({hue: 'blue'}));
-    $("#three").css("fill", randomColor({hue: 'blue'}));
-    $("#four").css("fill", randomColor({hue: 'blue'}));
-    $("#five").css("fill", randomColor({hue: 'blue'}));
-    $("#six").css("fill", randomColor({hue: 'blue'}));
-    $("#seven").css("fill", randomColor({hue: 'blue'}));
-    $("#eight").css("fill", randomColor({hue: 'blue'}));
 
 });
 });
